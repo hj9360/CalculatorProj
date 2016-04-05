6a891044487ffe1d8242bdf8f4685090c1dbfdf8
user <user@user159>
2016-04-05 18:53:08 +0900

    Complete sub method

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,10 +8,15 @@ public class Calculator {
 	public int add(int x, int y){
 		return x+y;
 	}
+	
+	public int sub(int x, int y){
+		return x-y;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculator cal = new Calculator();
 		System.out.println(cal.add(1,2));
+		System.out.println(cal.sub(2, 1));
 	}
 
 }
