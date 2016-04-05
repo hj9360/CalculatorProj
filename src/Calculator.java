65b245c1725fd38005b9e79b4d78aeca230e0777
user <user@user159>
2016-04-05 19:17:27 +0900

    Complete multiply method

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,11 +12,16 @@ public class Calculator {
 	public int sub(int x, int y){
 		return x-y;
 	}
+	
+	public int multiply(int x, int y) {
+		return x*y;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculator cal = new Calculator();
 		System.out.println(cal.add(1,2));
 		System.out.println(cal.sub(2, 1));
+		System.out.println(cal.multiply(2, 5));
 	}
 
 }
