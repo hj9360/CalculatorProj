2d489d7be2968bad85448cd0b37877c2c333c921
user <user@user159>
2016-04-05 18:41:28 +0900

    Create Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+
+public class Calculator {
+
+	public Calculator() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator cal = new Calculator();
+	}
+
+}
